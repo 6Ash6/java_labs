bc6e6fb74b9422ec669244c46136ab09496bd878
Natalia Mashkarova <rct.mashkare@bsu.by>
2022-10-23 14:22:07 +0300

    lab2

--- src/bsu/rfct/course2/group9/Mashkareva/Consumeble.java
+++
@@ -1,5 +0,0 @@
-package bsu.rfct.course2.group9.Mashkareva;
-
-public interface Consumeble {
-    public  void consume();
-}
--- src/bsu/rfct/course2/group9/Mashkareva/Typable.java
+++
@@ -1,5 +0,0 @@
-package bsu.rfct.course2.group9.Mashkareva;
-
-public interface Typable {
-    public String getType();
-}
