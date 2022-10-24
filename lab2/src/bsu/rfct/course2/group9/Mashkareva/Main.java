05542bc1624fcb254d17071114101783ed41ff30
Natalia Mashkarova <rct.mashkare@bsu.by>
2022-10-25 02:00:16 +0300

    initial main

---
+++ lab2/src/bsu/rfct/course2/group9/Mashkareva/Main.java
@@ -0,0 +1,11 @@
+package bsu.rfct.course2.group9.Mashkareva;
+
+import javax.swing.*;
+
+public class Main {
+    public  static void main (String[] args){
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
